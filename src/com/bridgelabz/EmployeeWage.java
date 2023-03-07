ead240d4900625ccae6bb205ec39eaa80c4ca441
AnjaliRawke <anjalirawke21@gmail.com>
2023-03-07 16:16:17 +0530

    Added: UC7 Refactor Code to write class method to compute emp wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,20 +1,20 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-	public static void main(String[] args) {
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME=2;
-		int EMP_RATE_PER_HOUR = 20;
-		int NUM_OF_WORKING_DAYS=20;
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
+	public static final int EMP_RATE_PER_HOUR= 20;
+	public static final int NUM_OF_WORKING_DAYS =2;
+	public static final int MAX_HRS_IN_MONTH = 100;
+
+	public static int computeEmpWage() {
 		//variables
 		int empHrs = 0;
-		int empWage = 0;
-		int MonthlyWage;
+		int TotalempWage = 0;
 		int TotalempHrs=0;
-		int maxHrsInMonth=100;
 		int TotalWorkingDays=0;
 		//Computation
-		while(TotalempHrs < maxHrsInMonth && TotalWorkingDays < NUM_OF_WORKING_DAYS) {
+		while(TotalempHrs < MAX_HRS_IN_MONTH && TotalWorkingDays < NUM_OF_WORKING_DAYS) {
 			TotalWorkingDays++;
 			int empCheck =(int) (Math.floor(Math.random()*10)%3);
 			switch(empCheck)
@@ -31,8 +31,14 @@ public class EmployeeWage {
 			}
 			TotalempHrs=TotalempHrs+empHrs;
 		}
-		System.out.println("TotalEmpHrs: "+TotalempHrs);
-		empWage=TotalempHrs*EMP_RATE_PER_HOUR;
-		System.out.println("Monthly Wage: "+empWage);
+		System.out.println("Total Employee Hrs: "+TotalempHrs);
+		TotalempWage=TotalempHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Total employee Wage: "+TotalempWage);
+		return TotalempWage;
+
 	}
+	public static void main(String[] args) {
+		computeEmpWage();
+	}
+
 }
