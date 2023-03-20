c984adb9e3a37202b1fcf1b1755c02396e2687c5
AnjaliRawke <anjalirawke21@gmail.com>
2023-03-20 18:06:13 +0530

    Added: UC13 Store daily wage along with total wage

--- src/com/bridgelabz/CompanyEmpWage.java
+++ src/com/bridgelabz/CompanyEmpWage.java
@@ -1,13 +1,17 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+
 public class CompanyEmpWage {
 	// instance constants
 	final String COMPANY_NAME;
 	final int WAGE_PER_HR;
 	final int MAX_WORKING_DAYS;
 	final int MAX_WORKING_HRS;
+	private ArrayList<Integer> dailyWages;
 	// instance variable
 	int totalEmpWage;
+	int dailyWage;
 
 	CompanyEmpWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs)
 	{
@@ -16,6 +20,7 @@ public class CompanyEmpWage {
 		this.MAX_WORKING_DAYS = maxWorkingDays;
 		this.MAX_WORKING_HRS = maxWorkingHrs;
 		totalEmpWage = 0;
+		dailyWage = 0;
 	}
 
 	void setTotalEmployeeWage(int totalEmpWage)
@@ -23,14 +28,19 @@ public class CompanyEmpWage {
 		this.totalEmpWage = totalEmpWage;
 	}
 
+	void setDailyWage(int dailyWage){
+		this.dailyWage = dailyWage;
+	}
+
 	@Override
 	public String toString()
 	{
 		System.out.println("Details of " + COMPANY_NAME + " employee");
 		System.out.println("-----------------------------------------------------");
-		System.err.println("Wage per hour:" + WAGE_PER_HR);
+		System.out.println("Wage per hour:" + WAGE_PER_HR);
 		System.out.println("Maximum working days:" + MAX_WORKING_DAYS);
 		System.out.println("Maximum working hours:" + MAX_WORKING_HRS);
+		System.out.println("Daily Wage : " + dailyWage);
 		return "Total wage for a month of " + COMPANY_NAME + " employee is " + totalEmpWage + "\n";
 	}
 }
